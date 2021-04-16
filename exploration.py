3857c74b3ce6a941879396af3611ec45dbaa16f5
Matt B <14bewleym@gmail.com>
2021-04-16 14:38:39 -0400

    Added creation of sqlite db from csv

--- exploration.py
+++ exploration.py
@@ -39,6 +39,7 @@ import webbrowser
 import folium
 import os
 import re
+import sqlalchemy
 
 # %% some initial settings
 generate_maps = 0
@@ -66,6 +67,21 @@ logging.info(f"Reading in data from {data_path}")
 # read in in chunks for big file
 chunk_size = 1.5 *  (10 ** 6) # number of rows to read in at a time (or once, as the case currently is)
 buspositions = pd.DataFrame()
+# trying to read in dataset with sql
+dbname = "buspositions.db"
+db = sqlalchemy.create_engine("sqlite:///" + dbname)
+if not os.path.exists(dbname): # if db doesn't exist then create one
+    indexvalue = 0 # iterator for setting dataframe index
+    for df in pd.read_csv(data_path, chunksize=chunk_size, iterator=True):
+        df.index += indexvalue
+
+        df.to_sql("buspositions", db, if_exists="append")
+        indexvalue = df.index[-1] + 1
+
+        print(f"Loaded rows up to index {indexvalue}")
+
+#buspositions = pd.read_sql_query("SELECT * FROM buspositions WHERE has_service=True", db)
+
 cols = ["timestamp","vehicle_id", "route_number", "run_number", "headsign", "has_service", "lon", "lat"]
 with pd.read_csv(data_path, chunksize=chunk_size) as reader:
     iterator = 0
